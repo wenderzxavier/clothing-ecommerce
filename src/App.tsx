c3b1fa32c3dfc9c2d26f933e369a6384d98228d0
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2020-11-04 13:14:19 -0300

    feat: implement nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 import './App.scss'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={Shop} />
