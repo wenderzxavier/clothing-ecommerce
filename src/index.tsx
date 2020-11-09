cd61ff247e858118cbe1288480a4e08b23ffae14
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2020-11-09 17:47:26 -0300

    feat: adds persistance on redux cart data

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/root-reducer.tsx
+++ src/redux/root-reducer.tsx
@@ -1,11 +1,21 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
+import { persistReducer } from "redux-persist";
 
-export const rootReducer = combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 })
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
 export type RootState = ReturnType<typeof rootReducer>;
+
+export default persistReducer(persistConfig, rootReducer); 
